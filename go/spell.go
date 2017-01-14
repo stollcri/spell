859e2325f2947d9d5166d0738f68fdfdfd560f66
Christopher Stoll <stollcri@gmail.com>
2017-01-14 10:54:27 -0500

    Started go version of spell

---
+++ go/spell.go
@@ -0,0 +1,83 @@
+/**
+ * spell -- utility suggest the correct spelling for words (groups of letters)
+ *  https://github.com/stollcri/spell
+ *
+ *
+ * Copyright (c) 2017, Christopher Stoll
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of spell nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func bestmatch(word string, wordlist []string) string {
+	return word
+}
+
+func main() {
+	showVersion := flag.Bool("version", false, "Version information")
+	flag.Parse()
+	// var spellWords string = flag.Args()
+	// above is the same as below, type is inferred
+	spellWords := flag.Args()
+
+	if *showVersion {
+		fmt.Println("spell 0.1");
+		fmt.Println("Copyright 2017, Christopher Stoll");
+		fmt.Println("https://github.com/stollcri/spell");
+		os.Exit(0)
+	}
+
+	configFile := ".spell"
+	if os.Getenv("SPELL_FILE") != "" {
+		configFile = os.Getenv("SPELL_FILE")
+	}
+	dat, err := ioutil.ReadFile(configFile)
+	check(err)
+	wordList := strings.Split(string(dat), "\n")
+
+	for i := 0; i < len(spellWords); i++ {
+		recomendedstring := bestmatch(spellWords[i], wordList)
+		fmt.Println(recomendedstring)
+	}
+
+	os.Exit(0)
+}
